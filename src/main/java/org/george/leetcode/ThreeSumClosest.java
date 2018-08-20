7e73c7620fffc2ab0b34b668cb3f76464745cc20
gang99zheng@gmail.com <Milli0ns>
2018-08-19 20:11:46 -0500

    Add LetterCombinations of phone number

--- src/main/java/org/george/leetcode/ThreeSumClosest.java
+++ src/main/java/org/george/leetcode/ThreeSumClosest.java
@@ -1,8 +1,31 @@
 package org.george.leetcode;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
     public static int threeSumClosest(int[] nums, int target) {
-        int result = 2;
+        int result = 10000;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++) {
+                int lo = i + 1, hi = nums.length - 1;
+                while (lo < hi) {
+                    int sum = nums[i] + nums[lo] + nums[hi];
+                    System.out.println(nums[i] + "," + nums[lo] + "," + nums[hi]);
+                    if(Math.abs(sum-target) < Math.abs(result-target)) {
+                        result = sum;
+//                    } else {
+//                        break;
+                    }
+                    System.out.println("result=" + result);
+                    if (sum > target) {
+                        hi--;
+                    } else if (sum < target) {
+                        lo++;
+                    } else {
+                        return result;
+                    }
+                }
+        }
         return result;
     }
 }
--- src/test/java/org/george/leetcode/ThreeSumClosestTest.java
+++ src/test/java/org/george/leetcode/ThreeSumClosestTest.java
@@ -10,7 +10,18 @@ import static junit.framework.Assert.assertEquals;
 
 public class ThreeSumClosestTest {
     @Test
-    public void test21() {
-        assertEquals(2, ThreeSumClosest.threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+    public void test() {
+        assertEquals(2, ThreeSumClosest.threeSumClosest(new int[]{-1, 2, 1, -4,7}, 1));
+        assertEquals(-3, ThreeSumClosest.threeSumClosest(new int[]{-1, 2, 1, -4,2}, -2));
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(0, ThreeSumClosest.threeSumClosest(new int[]{0,2,1,-3}, 1));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(82, ThreeSumClosest.threeSumClosest(new int[]{1,2,4,8,16,32,64,128}, 82));
     }
 }
